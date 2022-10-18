23146adfce37c76a5755d6e65163d54ca5eb6fcf
hoganrsherrow <hogan.r.sherrow@vanderbilt.edu>
2022-10-17 21:33:54 -0500

    car rotations

--- assets/js/Car.js
+++ assets/js/Car.js
@@ -9,11 +9,17 @@ class Car {
         this.acceleration = 0.2;
         this.maxSpeed = 3;
         this.friction = 0.05;
+        this.angle = 0;
 
         this.controls = new Controls();
     }
-    //This is what makes the car move
+
     update() {
+        this.#move();
+    }
+
+    //This is what makes the car move
+    #move() {
         if(this.controls.forward) {
             this.speed += this.acceleration;
         }
@@ -35,17 +41,40 @@ class Car {
         if(Math.abs(this.speed) < this.friction) {
             this.speed = 0;
         }
-        this.y -= this.speed;
+        if(this.controls.left) {
+            this.angle += 0.03;
+        }
+        if(this.controls.right) {
+            this.angle -= 0.03;
+        }
+
+        if(this.speed!=0){
+            const flip=this.speed>0?1:-1;
+            if(this.controls.left){
+                this.angle+=0.03*flip;
+            }
+            if(this.controls.right){
+                this.angle-=0.03*flip;
+            }
+        }
+
+        this.x = Math.sin(this.angle) * this.speed;
+        this.y = Math.cos(this.angle) * this.speed;
     }
     //Renders the car
     draw(ctx) {
+        //ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(-this.angle);
         ctx.beginPath();
         ctx.rect(
-            this.x - this.width/2,
-            this.y - this.height/2,
+            -this.width/2,
+            -this.height/2,
             this.width,
             this.height
         );
         ctx.fill();
+
+        ctx.restore();
     }
 }
