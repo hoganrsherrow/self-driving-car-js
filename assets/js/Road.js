4debae54b5e632041f9099cc46c6d0171b633756
hoganrsherrow <hogan.r.sherrow@vanderbilt.edu>
2022-11-07 21:31:46 -0600

    Lane creation

---
+++ assets/js/Road.js
@@ -0,0 +1,42 @@
+class Road {
+    constructor(x, width, laneCount = 3) {
+        this.x = x;
+        this.width = width;
+        this.laneCount = laneCount;
+        this.left = x - width / 2;
+        this.right = x + width / 2;
+
+        const infinity = 1000000;
+        this.top = -infinity;
+        this.bottom = infinity;
+    }
+
+    draw(ctx) {
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = "white";
+
+        for(let i = 0; i <= this.laneCount; i++) {
+            // linear interpolation
+            const x = lerp(
+                this.left,
+                this.right,
+                i / this.laneCount
+            );
+            ctx.beginPath();
+            ctx.moveTo(x, this.top);
+            ctx.lineTo(x, this.bottom);
+            ctx.stroke();
+        }
+        
+
+        // ctx.beginPath();
+        // ctx.moveTo(this.right, this.top);
+        // ctx.lineTo(this.right, this.bottom);
+        // ctx.stroke();
+    }
+}
+
+// Linear Interpolation
+function lerp(A, B, t) {
+    return A + (B - A) * t;
+}
--- assets/js/index.js
+++ assets/js/index.js
@@ -2,17 +2,19 @@
 const canvas = document.getElementById("canvas");
 canvas.width = 200;
 
+const ctx = canvas.getContext("2d");
+const road = new Road(canvas.width / 2, canvas.width * 0.93);
+const car = new Car(100, 100, 30, 50);
+//car.draw(ctx);
+
 //Define animation
 const animate = () => {
     car.update();
     canvas.height = window.innerHeight;
+    road.draw(ctx);
     car.draw(ctx);
     requestAnimationFrame(animate);
 }
-
-const ctx = canvas.getContext("2d");
-const car = new Car(100, 100, 30, 50);
-car.draw(ctx);
 
 animate();
 
